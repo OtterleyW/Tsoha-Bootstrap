fdff0a187458ff3902435273597f93e1e534631e
Nurmi Jenni K <jknurmi@da5-cs-bk107-03.helsinki.fi>
2016-04-04 11:41:05 +0300

    Lisätty create tables, drop tables ja add test data lauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,15 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+-- Kayttaja-taulun testidata
+INSERT INTO Kayttaja (name, email, password) VALUES ('Kalle Keräilijä', 'kkeraaja@email.com', 'Keräilijä1');
+INSERT INTO Kayttaja (name, email, password) VALUES ('Hanna Hamstaaja', 'hhamstraaja@email.com', 'Hamstraaja1');
+
+-- Kohde-taulun testidata
+
+INSERT INTO Kohde (owner_id, name, description, status, added) VALUES ('1', 'Kolikko', 'Hieno kiiltävä kolikko', 'avoinna', NOW());
+INSERT INTO Kohde (owner_id, name, description, status, added) VALUES ('2', 'Maljakko', 'Hieno kiiltävä maljakko', 'avoinna', NOW());
+
+
+-- Tarjous-taulun testidata
+
+INSERT INTO Tarjous (sender_id, reciever_id, item_id, description, offer_type, sent) VALUES ('1','2','1','Tämä on hieno tarjous','vastatarjous',NOW());
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,26 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  email varchar(50) NOT NULL,
+  password varchar(50) NOT NULL
+);
+
+CREATE TABLE Kohde(
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES Kayttaja(id),
+  name varchar(50) NOT NULL,
+  description varchar(400),
+  status varchar(50),
+  added DATE
+);
+
+CREATE TABLE Tarjous(
+  id SERIAL PRIMARY KEY,
+  sender_id INTEGER REFERENCES Kayttaja(id),
+  reciever_id INTEGER REFERENCES Kayttaja(id),
+  item_id INTEGER REFERENCES Kohde(id),
+  description varchar(400),
+  offer_type varchar(50),
+  sent DATE
+);
