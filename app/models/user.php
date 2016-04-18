04cb67f73003afe80fad811c781da4d438e38ad9
Nurmi Jenni K <jknurmi@da5-cs-bk107-03.helsinki.fi>
2016-04-18 11:28:25 +0300

    Lisätty sisäänkirjautuminen

---
+++ app/models/user.php
@@ -0,0 +1,41 @@
+<?php
+
+class User extends BaseModel {
+    public $id, $name, $email, $password;
+
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
+
+    public static function authenticate($name, $password) {
+        $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE name = :name AND password = :password LIMIT 1');
+        $query->execute(array('name' => $name, 'password' => $password));
+        $row = $query->fetch();
+        if ($row) {
+            $user = new User(array(
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'email' => $row['email'],
+            ));
+            return $user;
+        } else {
+            return null;
+        }
+    }
+    
+     public static function find($id) {
+        $query = DB::connection()->prepare('SELECT * FROM Kayttaja WHERE id = :id LIMIT 1');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+        if ($row) {
+            $user = new User(array(
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'email' => $row['email'],
+            ));
+            return $user;
+        }
+        return null;
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -8,8 +8,11 @@ $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
 });
 
-$routes->get('/login', function() {
-    HelloWorldController::login();
+$routes->get('/login', function(){
+  UserController::login();
+});
+$routes->post('/login', function(){
+  UserController::handle_login();
 });
 
 $routes->get('/item_list', function() {
